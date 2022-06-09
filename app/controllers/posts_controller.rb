b3abdbcdc998c8551186a80dba446969783c0388
cynthiainga <candyinga67@gmail.com>
2022-06-09 06:31:44 +0200

    Add a logic to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,29 @@ class PostsController < ApplicationController
     @user = current_user
   end
 
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to { user_posts(current_user) }
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    a_post = params.require(:post).permit(:title, :text)
+    a_post[:author] = current_user
+    a_post
+  end
+
+  def post_params_setu
+    Post.create(author: current_user, title: params[:post][:title], text: params[:post][:text])
   end
 end
