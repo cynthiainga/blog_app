862bf7b9b215d55fcd4e28827982100ed9b75bea
cynthiainga <candyinga67@gmail.com>
2022-06-09 07:00:01 +0200

    Add some tests to check comments routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,5 +6,5 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     Like.creat(author: current_user, post:)
-    redirect_to use_post_path(current_user, post)
+    redirect_to user_post_path(current_user, post)
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe "Comments", type: :request do
     end
   end
 
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/comments/index'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/comments/create'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
