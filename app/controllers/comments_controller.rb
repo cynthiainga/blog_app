957ed6a9a1bc5bce35564657a1f77e98ce5e6cf7
cynthiainga <candyinga67@gmail.com>
2022-06-09 06:25:47 +0200

    Add a logic to create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+    @user = current_user
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    Comment.create(post_params(post))
+    redirect_to user_post_path(current_user, post)
+  end
+
+  private
+
+    def post_params
+      a_post = params.require(:comment).permit(:text)
+      a_post[:author] = current_user
+      a_post[:author] = post
+      a_post
+    end
 end
