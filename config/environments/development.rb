05f9da8793de8e41cbcb643cb2b3b11f3c57f8b1
cynthiainga <candyinga67@gmail.com>
2022-06-13 14:27:15 +0200

    Make some modifications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Preview email using letter-opener in development
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
