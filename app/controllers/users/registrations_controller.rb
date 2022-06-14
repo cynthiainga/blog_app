2a97d8d7533beeb05c01787ff76bdaa138adcf8d
cynthiainga <candyinga67@gmail.com>
2022-06-14 11:06:19 +0200

    Generate devise:views

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,9 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+
   protected
+  
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
