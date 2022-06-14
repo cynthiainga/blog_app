03e9bb3a38ee8cef70af97b660363e77de6bab72
Thermiee <navyblack16@yahoo.com>
2022-06-14 10:29:46 +0100

    fix linter errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
   protected
-  
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
