7f19d3bbb9f2030306cc8945c11802d52506d4a5
cynthiainga <candyinga67@gmail.com>
2022-06-14 19:32:18 +0200

    Fix linters errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :config_sign_up_params, only: [:create]
+
   protected
+
   def config_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
