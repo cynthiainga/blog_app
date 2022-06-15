6ef9d90b1c2cc3344003b21e4815d1eb0f71dba2
cynthiainga <candyinga67@gmail.com>
2022-06-15 11:51:35 +0200

    Add some lodic on posts forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"Add a role column to the users table
+  root "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
