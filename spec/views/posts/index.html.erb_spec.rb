f194fb2f55bd69abe9f7e5b5077a0b2e8397a7f3
Thermiee <navyblack16@yahoo.com>
2022-06-16 14:48:14 +0100

    add show test

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
-  describe 'Index page' do
+  context 'Index page' do
     before(:each) do
       @users = User.create!(name: 'Candy', photo: '#photo_candy', bio: 'bio',
                             email: 'test@domain.com')
       @user.confirm
 
       @post = Post.create(author_id: @user.id, title: 'Integration test', text: 'Exciting!')
-      Comment.create(author_id: @user.id, post_id: @post.id, text: 'Test Comment')
+      @comment = Comment.create(author_id: @user.id, post_id: @post.id, text: 'Test Comment')
       Like.create(author_id: @user.id, post_id: @post.id)
 
       visit user_post_path(@user.id, @post.id)
@@ -23,38 +23,38 @@ RSpec.describe 'Post', type: :system do
     end
 
     it 'should show post title' do
-        expect(page).to have_content(@post.title)
+      expect(page).to have_content(@post.title)
     end
 
     it "should show the post's body." do
-        expect(page).to have_content(@post.text)
+      expect(page).to have_content(@post.text)
     end
 
     it 'should show the first comments on a post' do
-        expect(page).to have_content('Test Comment')
+      expect(page).to have_content('Test Comment')
     end
 
     it 'should display the number of comments the user has written' do
-        expect(page).to have_content('Number of comments:')
-        expect(page).to have_content(@user.comments_counter)
+      expect(page).to have_content('Number of comments:')
+      expect(page).to have_content(@user.comments_counter)
     end
 
     it 'should display how many comments a post has.' do
-        expect(page).to have_content('Number of comments:')
-        expect(page).to have_content(@post.comments_count)       
+      expect(page).to have_content('Number of comments:')
+      expect(page).to have_content(@post.comments_count)
     end
 
     it 'should display how many likes a post has.' do
-        expect(page).to have_content('Number of likes:')
-        expect(page).to have_content(@post.likes_count)
+      expect(page).to have_content('Number of likes:')
+      expect(page).to have_content(@post.likes_count)
     end
 
     it 'should show see a section for pagination if there are more posts than fit on the view.' do
-        expect(page).to have_content('See All Posts')
+      expect(page).to have_content('See All Posts')
     end
 
     it "should redirect to the post's show page when I click on a post" do
-         click_link(@post.title)
+      click_link(@post.title)
       expect(current_path).to eql(user_post_path(@user.id, @post.id))
     end
   end
---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Post', type: :system do
+  context 'show page' do
+    before(:each) do
+      @users = User.create!(name: 'Candy', photo: '#photo_candy', bio: 'bio',
+                            email: 'test@domain.com')
+      @user.confirm
+
+      @post = Post.create(author_id: @user.id, title: 'Integration test', text: 'Exciting!')
+      @comment = Comment.create(author_id: @user.id, post_id: @post.id, text: 'Test Comment')
+      Like.create(author_id: @user.id, post_id: @post.id)
+
+      visit user_post_path(@user.id, @post.id)
+    end
+
+    it "should show post's title" do
+      expect(page).to have_content(@post.title)
+    end
+
+    it "should show post's author" do
+      expect(page).to have_content(@post.author.name)
+    end
+
+    it 'should show the number of comments' do
+      expect(page).to have_content('Comments 1')
+    end
+
+    it 'should show the number of likes' do
+      expect(page).to have_content('Likes 0')
+    end
+
+    it 'should show the post body' do
+      expect(page).to have_content(@post.text)
+    end
+
+    it 'should show the username of each commentor' do
+      expect(page).to have_content(@comment.author.name)
+    end
+
+    it 'should show the text comment each commentor' do
+      expect(page).to have_content(@comment.text)
+    end
+  end
+end
